6fc17ed5fd920794a1b2a7611b01cacf3b80a4d5
valentin Mariot <valentinmariot@gmail.com>
2022-09-27 23:15:44 +0200

    (first-post.js): Head and Script concepts

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,26 @@
 import Link from 'next/link';
+import Script from 'next/script';
+import Head from 'next/head';
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
-  );
-}
+    return (
+      <>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </>
+    );
+  }
